79707831dcc3d8fcec1b768056f209e4846ce641
tiepnv <tiepnv@lottefinance.com>
2025-06-20 10:59:59 +0700

    Update indẽ and app route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -24,7 +24,7 @@ import Transaction from "@/pages/Onboard/Transaction";
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path={Router.ONBOARDING} element={<OnboardingLayout />}>
+      <Route path={"/"} element={<OnboardingLayout />}>
         {/* <Route index element={<Onboarding />} /> */}
 
         <Route path="" element={<CustomerVerifyIndex />}>
